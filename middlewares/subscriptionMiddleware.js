8f50e651d05cba2d54450f73f843e7dc33b2a2d3
Cedar Daniel <cedardaniel1998@gmail.com>
2023-12-21 11:04:29 +0100

    fixed file upload

--- middlewares/subscriptionMiddleware.js
+++ middlewares/subscriptionMiddleware.js
@@ -23,8 +23,8 @@ exports.calculateAmount = catchAsync(async (req, res, next) => {
 });
 
 exports.uploadAttachments = catchAsync(async (req, res, next) => {
-  if (req.body.attachments) {
-    const attachments = await handleUpload(req.body.attachments);
+  if (req.files.attachments) {
+    const attachments = await handleUpload(req.files.attachments);
 
     req.body.attachments = [attachments];
   }
--- utils/upload.js
+++ utils/upload.js
@@ -1,9 +1,18 @@
-const stream = require('stream');
+const path = require('path');
 
 const cloudinary = require('./cloudinary');
+const datauri = require('datauri/parser');
+
+const dUri = new datauri();
+
+const getDataUri = (fileName, buffer) =>
+  dUri.format(path.extname(fileName).toString(), buffer);
 
 const upload = async (file) => {
-  const response = await cloudinary.uploader.upload(file);
+  const { name, data } = file;
+
+  const datauri = getDataUri(name, data);
+  const response = await cloudinary.uploader.upload(datauri.content);
   return response.secure_url;
 };
 
