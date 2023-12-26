8b7024f1db3037d87db43be76dfc735fa60ec491
Cedar Daniel <cedardaniel1998@gmail.com>
2023-12-26 18:42:23 +0100

    set referral link to random 5 digit number

--- utils/generateReferralLink.js
+++ utils/generateReferralLink.js
@@ -1,6 +1,6 @@
 function generateRandom10DigitNumber() {
-  const min = 1000000000; // Minimum 10-digit number
-  const max = 9999999999; // Maximum 10-digit number
+  const min = 10000; // Minimum 5-digit number
+  const max = 99999; // Maximum 5-digit number
 
   // Generate a random number between min and max
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
